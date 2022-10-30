bf228113aab719043ad7cd5cf93c7d2899395102
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2022-10-29 23:14:56 -0300

    myPlayer Tennis Return fixed

--- pages/reviews/tennis.js
+++ pages/reviews/tennis.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from "../../styles/Home.module.css";
 
 const url = "http://localhost:3000/api/players/reviews";
 
@@ -8,15 +9,38 @@ export default function Tennis() {
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((result) => setPlayerName(result));
+      .then((result) => setPlayerName(result.data));
   }, []);
 
   console.log(playerName);
 
   return (
     <div>
-      {playerName &&
-        playerName.map((player) => <div key={player._id}>{player.name}</div>)}
+      <h3 className={styles.title}>My Teenis Players Reviews</h3>
+      <br />
+      <div className={styles.grid}>
+        {playerName &&
+          playerName.map((player) => (
+            <div className={styles.card} key={player._id}>
+              <span>Player: {player.name}</span>
+              <br />
+              <span>Age: {player.age}</span>
+              <br />
+              <span>Hand: {player.hand}</span>
+              <br />
+              <span>Plays: {player.plays}</span>
+              <br />
+              <span>Service: {player.sacks}</span>
+              <br />
+              <span>Hability: {player.hability}</span>
+              <br />
+              <span>Phisycal: {player.physical}</span>
+              <br />
+              <span>Rating: {player.rate}</span>
+              <br />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
